c07be2048d21dbf971810dfadbcc6bb2291b6aff
Jpenderdragon <Jpenderdragon@gmail.com>
2018-12-22 11:01:13 -0500

    We just about finished the phaser game.

--- platformer/game.js
+++ platformer/game.js
@@ -1,9 +1,12 @@
-let pl, keys
+let pl, keys, plats, coins, over, bad
 
 
 
 function preload() {
   this.load.image('bg',  './assets/background.jpg')
+  this.load.image('bad',  './assets/Slime.png')
+  this.load.image('pf',  './assets/platform.png')
+  this.load.image('coin',  './assets/Coin.png')
   this.load.spritesheet('hero','./assets/hero.png',
    {frameWidth: 14, frameHeight: 19}
   )
@@ -16,10 +19,63 @@ function create()  {
   pl = this.physics.add.sprite(100,100, 'hero')
   pl.setCollideWorldBounds(true)
   //player.setBounce(1) // between 0 and 1
-  keys = this.input.keyboard.createCursorKeys()
+  
   pl.setScale(3)
+  pl.setGravityY(1200)
+  
+  bad = this.physics.add.sprite(430,100, 'bad')
+  bad.setCollideWorldBounds(true)
+  bad.setScale(3)
+  bad.setBounce(1)
+  bad.setVelocity(200)
+
+  
+  plats = this.physics.add.staticGroup()
+  plats.create(300,300, 'pf').setScale(5,4).refreshBody()
+  plats.create(400,240, 'pf').setScale(5,4).refreshBody()
+  plats.create(200,350, 'pf').setScale(5,4).refreshBody()
+  plats.create(450,300, 'pf').setScale(5,4).refreshBody()
+  plats.create(500,350, 'pf').setScale(5,4).refreshBody()
+  
+  coins = this.physics.add.staticGroup()
+  coins.create(300,260, 'coin').setScale(3,3)
+  coins.create(380,200, 'coin').setScale(3,3)
+  coins.create(110,23, 'coin')
+ 
+  scoreText = this.add.text(16,16, 'Score: 0')
+  let score = 0
+
+  function collectCoin(pl,coin) {
+   score += 1
+    scoreText.setText(`Score: ${score}`)
+    coin.destroy()
+  }
+   
+
+  
 
  
+  function hitbad(pl,bad) {
+    bad.destroy()
+    this.add.image(0,0, 'over').setOrigin(0,0)
+    over = true
+  }
+ 
+  this.physics.add.collider(pl,plats)
+  this.physics.add.collider(bad,plats)
+  this.physics.add.collider(pl,coins, collectCoin)
+  this.physics.add.collider(pl,  bad, hitbad, null, this)
+  
+  
+ 
+  
+
+  
+  
+  
+  
+  
+  keys = this.input.keyboard.createCursorKeys()
 }
 
 
@@ -43,6 +99,12 @@ if (pl.body.onFloor()) {
    pl.setVelocityY(-400)
   }
 }
+    if (over && keys.space.isDown) {
+     over = false
+     this.create()
+    }
+
+
 
 }
 
@@ -58,13 +120,8 @@ let config = {
     create: create,
     update: update, 
   },
-  physics: {
-    default: 'arcade',
-    arcade: {
-      gravity: { y: 1200, },
-      debug: true
-    }
-  },
+  physics: { default: 'arcade'},
+  
 }
 
 new Phaser.Game(config)
